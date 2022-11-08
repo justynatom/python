bb4c7874b66c7d26f8ab1ab599ec1ff48311f896
justynatom <justynaxtomaszewska@gmail.com>
2022-11-08 23:15:03 +0100

    zadanie domowe z lekcji 8

--- 08_moduły/zad_dom_06/zad_06.py
+++ 08_moduły/zad_dom_06/zad_06.py
@@ -22,15 +22,35 @@ def generate_sequence(seq_length):
     for i in range(seq_length):
         random_sign = random.choice(tab_number)
         seq= seq + str(random_sign)
-        print(seq)
+    print(seq)
     return seq
 
+# def is_sequence_valid_old(longest_seq):
+#     if len(longest_seq) >= 2:
+#         print(longest_seq)
+#     else:
+#         new_gen_seq = generate_sequence(25)
+#         tmp, len_tmp = find_longest_sequence(new_gen_seq)
+#     return new_gen_seq, tmp, len_tmp
+
+def is_sequence_valid(generated_seq):
+    tmp, len_tmp = find_longest_sequence(generated_seq)
+    while len_tmp < 2:
+        len_generated_seq = len(generated_seq)
+        print("wrong seq: ", generated_seq)
+        new_gen_seq = generate_sequence(len_generated_seq)
+        generated_seq = new_gen_seq
+        tmp, len_tmp = find_longest_sequence(generated_seq)
+    return generated_seq
 
 def main():
     var = 'banannnnannnnnnnnnanananananaaaana'
     print(find_longest_sequence(word=var))
-    new_seq = generate_sequence(25)
-    print(find_longest_sequence(word=new_seq))
+    seq_len_user = 10
+    new_seq = generate_sequence(seq_len_user)
+    new_seq_validated = is_sequence_valid(generated_seq=new_seq)
+    print("good seq: ", new_seq_validated)
+    print(find_longest_sequence(word=new_seq_validated))
 
 main()
 
